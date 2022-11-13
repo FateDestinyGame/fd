76e552f9a63db8f1efe4f8772a4433ec943091b5
Ruben Cesario <rubencesario.rc@gmail.com>
2022-11-13 16:12:05 +0100

    test pr

--- lib/main.dart
+++ lib/main.dart
@@ -13,6 +13,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(primarySwatch: Colors.green),
       home: const RootPage(),
       debugShowCheckedModeBanner: false,
+      //test pr
     );
   }
 }
